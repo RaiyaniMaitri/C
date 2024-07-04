d0aa199c47c8139fb232d2d793f203372e0da065
Maitree <maitriraiyani7@gmail.com>
2024-07-04 19:08:12 +0530

    Add files via upload

---
+++ lab16_c1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+	int m,n;
+	printf("enter a size of array m and n:");
+	scanf("%d %d",&m,&n);
+	if(m==n){
+		printf("matrix is squre");
+	}
+	else{
+		printf("matrix is not squre");
+	}
+}
